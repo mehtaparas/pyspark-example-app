1ca39f37f7ea81be4419dab421fbc13c2b682c91
paras.mehta <mehtaparas77@gmail.com>
2019-03-17 18:23:35 -0400

    added count_per_skill logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, col
 
 def count_per_skill(df):
     """
@@ -6,15 +7,19 @@ def count_per_skill(df):
     :return : dataframe with two columns - 1) skill name, 2) distinct # of people who have that skill
     """
     # Write function logic here
+    skills_count = (df.select(
+        col("name"),
+        explode(col("technical_skills")).alias("skill_name")
+    ).groupBy("skill_name").agg({"name": "count"}))
 
-    # return skills_count
+    return skills_count
 
 
 if __name__ == '__main__':
-    spark = SparkSession.builder.appName("pyspark-example-app")
+    spark = SparkSession.builder.appName("pyspark-example-app").getOrCreate()
 
     input_df = spark.read.json("data")
 
-    skills_count_df = count_per_skill(df)
+    skills_count_df = count_per_skill(input_df)
 
     skills_count_df.write.csv("output_data")
