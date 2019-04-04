208619eb4fbbc56a9fa7a68adbbbb4883d7191e3
Jeffrey Box <jeffreylbox@gmail.com>
2019-04-04 15:13:17 -0400

    jbox pull request - workshop 1 (#6)
    * moved logic to main() function
    
    * added instructions for keeping local forked repos in sync with the original repo and for creating pull requests
    
    * added repo overview
    
    * reworded repo overview
    
    * added reference link for creating a pull request
    
    * Workshop1 (#5)
    
    * Kendra json file and README edit (#2)
    
    * added instructions for keeping local forked repos in sync with the original repo and for creating pull requests
    
    * added repo overview
    
    * reworded repo overview
    
    * added reference link for creating a pull request
    
    * Add data (#3)
    
    * Added chen.json
    
    * adding my skills to the data
    
    * added instructions for keeping local forked repos in sync with the original repo and for creating pull requests
    
    * added repo overview
    
    * reworded repo overview
    
    * added reference link for creating a pull request
    
    * Adding Yissel's skills to the project (#4)
    
    * added pytest and pyspark as required dependencies

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from pyspark.sql.functions import explode, col
 def count_per_skill(df):
     """
     :param df: dataframe with schema: [name: string, technical_skills: array<string>]
-    :return : dataframe with two columns - 1) skill name, 2) distinct # of people who have that skill
+    :return : dataframe with two columns - 1) skill name, 2) # of people who have that skill
     """
     # Write function logic here
-    skills_count = (df.select(
+    skills_count = df.select(
         col("name"),
         explode(col("technical_skills")).alias("skill_name")
-    ).groupBy("skill_name").agg({"name": "count"}))
+    ).groupBy("skill_name").agg({"name": "count"})
 
     return skills_count
 
 
-if __name__ == '__main__':
+def main():
     spark = SparkSession.builder.appName("pyspark-example-app").getOrCreate()
 
     input_df = spark.read.json("data")
@@ -23,3 +23,7 @@ if __name__ == '__main__':
     skills_count_df = count_per_skill(input_df)
 
     skills_count_df.write.csv("output_data")
+
+
+if __name__ == '__main__':
+    main()
