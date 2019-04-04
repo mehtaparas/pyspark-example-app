a358a1a06f0ed14541009253ffdc0698d05aa244
paras.mehta <mehtaparas77@gmail.com>
2019-04-03 23:15:16 -0400

    created package for dataframe transform functions and refactored count_per_skill from main

---
+++ dataframe_utils/transforms.py
@@ -0,0 +1,16 @@
+from pyspark.sql.functions import col, explode
+
+
+def count_per_skill(df):
+    """
+    :param df: dataframe with schema: [name: string, technical_skills: array<string>]
+    :return : dataframe with two columns - 1) skill name, 2) distinct # of people who have that skill
+    """
+    # Write function logic here
+    skills_count = (df.select(
+        col("name"),
+        explode(col("technical_skills")).alias("skill_name")
+    ).groupBy("skill_name").agg({"name": "count"}).withColumnRenamed("count(name)", "count"))
+
+    return skills_count
+
--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, col
-
-def count_per_skill(df):
-    """
-    :param df: dataframe with schema: [name: string, technical_skills: array<string>]
-    :return : dataframe with two columns - 1) skill name, 2) distinct # of people who have that skill
-    """
-    # Write function logic here
-    skills_count = (df.select(
-        col("name"),
-        explode(col("technical_skills")).alias("skill_name")
-    ).groupBy("skill_name").agg({"name": "count"}))
-
-    return skills_count
-
+from dataframe_utils.transforms import count_per_skill
 
 if __name__ == '__main__':
     spark = SparkSession.builder.appName("pyspark-example-app").getOrCreate()
